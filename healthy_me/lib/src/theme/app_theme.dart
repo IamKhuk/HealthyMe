6f4f92d25576badc0ce6e329a08dc70cbb71a48a
IamKhuk <khukumovkhusan@gmail.com>
2022-01-02 14:14:01 +0500

    AppTheme added

---
+++ healthy_me/lib/src/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTheme {
+  /// Colors
+  static const Color orange = Color(0xFFED8950);
+  static const Color orangeLight = Color(0xFFFFF7EC);
+  static const Color bg = Color(0xFFF2F6FE);
+  static const Color black = Color(0xFF080A24);
+  static const Color dark = Color(0xFF656565);
+  static const Color gray = Color(0xFFE8E9FF);
+  static const Color light = Color(0xFFDEDEDE);
+  static const Color gold = Color(0xFFFDC350);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color purple = Color(0xFF585CE5);
+  static const Color purpleLight = Color(0xFF7376F4);
+  static const Color baseColor = Color(0xFF1C2A34);
+  static const Color highlightColor = Color(0xFF121212);
+
+
+  /// Fonts
+  static const String fontFamily = "Poppins";
+}

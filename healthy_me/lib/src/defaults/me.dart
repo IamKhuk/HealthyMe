5ccef601d62fffd9cdc84211d327fd2d7091b7e3
IamKhuk <khukumovkhusan@gmail.com>
2022-01-04 23:55:01 +0500

    Default me created

---
+++ healthy_me/lib/src/defaults/me.dart
@@ -0,0 +1,9 @@
+import 'package:healthy_me/src/model/me_model.dart';
+
+MeModel me = MeModel(
+  pfp: 'assets/images/pfp.jpg',
+  name: 'Khusan Khukumov',
+  email: 'khukumovkhusan@gmail.com',
+  password: '12345678',
+  location: false,
+);
---
+++ healthy_me/lib/src/model/me_model.dart
@@ -0,0 +1,15 @@
+class MeModel {
+  String pfp;
+  String name;
+  String email;
+  bool location;
+  String password;
+
+  MeModel({
+    required this.pfp,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.location,
+  });
+}

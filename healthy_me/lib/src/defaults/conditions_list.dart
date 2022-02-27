0117c12d78f3cd25589491247ed5782d734a87e1
IamKhuk <khukumovkhusan@gmail.com>
2022-02-27 17:06:36 +0500

    Conditions List

--- healthy_me/lib/src/defaults/conditions_list.dart
+++ healthy_me/lib/src/defaults/conditions_list.dart
@@ -1,5 +1,21 @@
 import 'package:healthy_me/src/model/conditions_model.dart';
 
 List<ConditionsModel> conditions = [
-
+  ConditionsModel(id: 1, name: 'Dry cough'),
+  ConditionsModel(id: 2, name: 'Aching body'),
+  ConditionsModel(id: 3, name: 'High temperature'),
+  ConditionsModel(id: 4, name: 'Sore throat'),
+  ConditionsModel(id: 5, name: 'Headache'),
+  ConditionsModel(id: 6, name: 'Difficulty sleeping'),
+  ConditionsModel(id: 7, name: 'Loss of appetite'),
+  ConditionsModel(id: 8, name: 'Diarrhoea'),
+  ConditionsModel(id: 9, name: 'Stomach cramps'),
+  ConditionsModel(id: 10, name: 'Stress'),
+  ConditionsModel(id: 11, name: 'Eyesight problems'),
+  ConditionsModel(id: 12, name: 'Feeling bloated'),
+  ConditionsModel(id: 13, name: 'Cannot poo'),
+  ConditionsModel(id: 14, name: 'Vomiting'),
+  ConditionsModel(id: 15, name: 'Farting a lot'),
+  ConditionsModel(id: 16, name: 'Feeling sick'),
+  ConditionsModel(id: 17, name: 'Earache'),
 ];

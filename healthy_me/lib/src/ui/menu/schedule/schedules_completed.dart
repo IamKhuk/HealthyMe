10dc19810dd2c536f9f7b2f3e51e9f44d7979b78
IamKhuk <khukumovkhusan@gmail.com>
2022-03-21 15:02:12 +0500

    Completed Schedules shimmer built

--- healthy_me/lib/src/ui/menu/schedule/schedules_completed.dart
+++ healthy_me/lib/src/ui/menu/schedule/schedules_completed.dart
@@ -4,6 +4,7 @@ import 'package:healthy_me/src/bloc/schedule_bloc.dart';
 import 'package:healthy_me/src/model/api/schedule_model.dart';
 import 'package:healthy_me/src/theme/app_theme.dart';
 import 'package:healthy_me/src/widgets/schedule_container.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CompletedSchedules extends StatefulWidget {
   @override
@@ -61,7 +62,139 @@ class _CompletedSchedulesState extends State<CompletedSchedules> {
                   )
                 : Container();
           } else {
-            return Container();
+            return Shimmer.fromColors(
+              child: ListView.builder(
+                itemCount: 10,
+                padding: EdgeInsets.only(
+                  bottom: 96,
+                  top: 76,
+                  left: 36,
+                  right: 36,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: 168,
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          border:
+                          Border.all(color: AppTheme.baseColor, width: 2),
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: AppTheme.baseColor,
+                                  ),
+                                ),
+                                SizedBox(width: 14),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      height: 16,
+                                      width: 120,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        color: AppTheme.baseColor,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Container(
+                                      height: 12,
+                                      width: 88,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        color: AppTheme.baseColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: 12,
+                                  width: 12,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: AppTheme.baseColor,
+                                  ),
+                                ),
+                                Container(
+                                  height: 12,
+                                  width: 120,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: AppTheme.baseColor,
+                                  ),
+                                ),
+                                Container(
+                                  height: 12,
+                                  width: 12,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: AppTheme.baseColor,
+                                  ),
+                                ),
+                                Container(
+                                  height: 12,
+                                  width: 72,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: AppTheme.baseColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: 38,
+                                  width: (MediaQuery.of(context).size.width -
+                                      116) /
+                                      2,
+                                  decoration: BoxDecoration(
+                                    color: AppTheme.baseColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                Container(
+                                  height: 38,
+                                  width: (MediaQuery.of(context).size.width -
+                                      116) /
+                                      2,
+                                  decoration: BoxDecoration(
+                                    color: AppTheme.baseColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      index == 9 ? Container() : SizedBox(height: 16),
+                    ],
+                  );
+                },
+              ),
+              baseColor: AppTheme.baseColor,
+              highlightColor: AppTheme.highlightColor,
+            );
           }
         },
       ),
